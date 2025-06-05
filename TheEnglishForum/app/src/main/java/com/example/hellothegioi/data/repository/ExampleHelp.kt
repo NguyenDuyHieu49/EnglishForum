8424bb21f9affcbc814e0f6f62831057e927f751
MinhVu272 <vuducminh27205@gmail.com>
2025-06-05 22:55:20 +0700

    Thêm mẫu hỗ trợ

---
+++ TheEnglishForum/app/src/main/java/com/example/hellothegioi/data/repository/ExampleHelp.kt
@@ -0,0 +1,10 @@
+package com.example.hellothegioi.data.repository
+
+object ExampleHelp {
+    public val  questions = listOf(
+    "Tôi quên tài khoản!" to "Để lấy lại tài khoản, vui lòng nhấn vào 'Quên mật khẩu' và làm theo hướng dẫn.",
+    "Tôi không thể đăng nhập!" to "Kiểm tra lại tên đăng nhập và mật khẩu, hoặc thử đổi mật khẩu nếu bạn quên.",
+    "Tôi muốn đăng ký!" to "Để đăng ký, hãy nhấn vào 'Đăng ký' trên màn hình đăng nhập.",
+    "Tôi không thể đăng bài!" to "Vui lòng kiểm tra lại kết nối mạng và thử lại. Nếu vấn đề vẫn còn, liên hệ với hỗ trợ kỹ thuật."
+)
+}
